cc606e461dc2a3b0248942af7146acfc74b19b87
Marc <marcantoine.lavigne.gauthier@gmail.com>
2025-06-07 20:12:19 -0400

    MVC facture

---
+++ menufact/facture/FactureController.java
@@ -0,0 +1,33 @@
+package menufact.facture;
+
+import menufact.Client;
+import menufact.facture.exceptions.FactureException;
+import menufact.plats.PlatChoisi;
+
+public class FactureController {
+    Facture facture;
+    FactureView factureView;
+
+    public FactureController(Facture modele, FactureView modeleView) {
+        this.facture = modele;
+        this.factureView = modeleView;
+    }
+
+    public void displayFacture() {
+        factureView.printFactureDetails(facture);
+
+    }
+
+    public void setClient(Client client) {
+        facture.associerClient(client);
+
+    }
+
+    public void addPlat(PlatChoisi plat) throws FactureException {
+        facture.ajoutePlat(plat);
+    }
+
+    public String getFacture() {
+        return facture.genererFacture();
+    }
+}
---
+++ menufact/facture/FactureView.java
@@ -0,0 +1,8 @@
+package menufact.facture;
+
+public class FactureView {
+
+    public void printFactureDetails(Facture facture){
+        System.out.println(facture.toString());
+    }
+}
