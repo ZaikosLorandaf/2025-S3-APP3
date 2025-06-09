17a980d0e0acf6373d000a22b463a051af05e257
Marc <marcantoine.lavigne.gauthier@gmail.com>
2025-06-09 11:13:22 -0400

    Update Facture.java

--- menufact/facture/Facture.java
+++ menufact/facture/Facture.java
@@ -2,8 +2,6 @@ package menufact.facture;
 
 import menufact.Client;
 import menufact.facture.exceptions.FactureException;
-import menufact.facture.states.FactureOpenState;
-import menufact.facture.states.FactureState;
 import menufact.plats.PlatChoisi;
 
 import java.util.ArrayList;
@@ -11,19 +9,19 @@ import java.util.Date;
 
 /**
  * Une facture du systeme Menufact
- *
  * @author Domingo Palao Munoz
  * @version 1.0
  */
 public class Facture {
     private Date date;
     private String description;
-    private FactureState factureState;
+    private FactureEtat etat;
     private ArrayList<PlatChoisi> platchoisi = new ArrayList<PlatChoisi>();
     private int courant;
     private Client client;
 
-    /********************** Constantes ************/
+
+    /**********************Constantes ************/
     private final double TPS = 0.05;
     private final double TVQ = 0.095;
 
@@ -31,17 +29,18 @@ public class Facture {
      *
      * @param client le client de la facture
      */
-    public void associerClient(Client client) {
+    public void associerClient (Client client)
+    {
         this.client = client;
     }
 
     /**
      * Calcul du sous total de la facture
-     *
      * @return le sous total
      */
-    public double sousTotal() {
-        double soustotal = 0;
+    public double sousTotal()
+    {
+        double soustotal=0;
         for (PlatChoisi p : platchoisi)
             soustotal += p.getQuantite() * p.getPlat().getPrix();
         return soustotal;
@@ -51,55 +50,60 @@ public class Facture {
      *
      * @return le total de la facture
      */
-    public double total() {
-        return sousTotal() + tps() + tvq();
+    public double total(){
+        return sousTotal()+tps()+tvq();
     }
 
     /**
      *
      * @return la valeur de la TPS
      */
-    private double tps() {
-        return TPS * sousTotal();
+    private double tps(){
+        return TPS*sousTotal();
     }
 
     /**
      *
      * @return la valeur de la TVQ
      */
-    private double tvq() {
-        return TVQ * (TPS + 1) * sousTotal();
+    private  double tvq(){
+        return TVQ*(TPS+1)*sousTotal();
     }
 
     /**
      * Permet de chager l'état de la facture à PAYEE
      */
-    public void payer() {
-        factureState.paid(this);
+    public void payer()
+    {
+        etat = FactureEtat.PAYEE;
     }
-
     /**
      * Permet de chager l'état de la facture à FERMEE
      */
-    public void fermer() {
-        factureState.close(this);
+    public void fermer()
+    {
+        etat = FactureEtat.FERMEE;
     }
 
     /**
      * Permet de changer l'état de la facture à OUVERTE
-     *
      * @throws FactureException en cas que la facture soit PAYEE
      */
-    public void ouvrir() throws FactureException {
-        factureState.open(this);
+    public void ouvrir() throws FactureException
+    {
+        if (etat == FactureEtat.PAYEE)
+            throw new FactureException("La facture ne peut pas être reouverte.");
+        else
+            etat = FactureEtat.OUVERTE;
     }
 
     /**
      *
      * @return l'état de la facture
      */
-    public FactureState getState() {
-        return factureState;
+    public FactureEtat getEtat()
+    {
+        return etat;
     }
 
     /**
@@ -108,7 +112,7 @@ public class Facture {
      */
     public Facture(String description) {
         date = new Date();
-        factureState = new FactureOpenState();
+        etat = FactureEtat.OUVERTE;
         courant = -1;
         this.description = description;
     }
@@ -118,8 +122,9 @@ public class Facture {
      * @param p un plat choisi
      * @throws FactureException Seulement si la facture est OUVERTE
      */
-    public void ajoutePlat(PlatChoisi p) throws FactureException {
-        if (factureState instanceof FactureOpenState)
+    public void ajoutePlat(PlatChoisi p) throws FactureException
+    {
+        if (etat == FactureEtat.OUVERTE)
             platchoisi.add(p);
         else
             throw new FactureException("On peut ajouter un plat seulement sur une facture OUVERTE.");
@@ -134,7 +139,7 @@ public class Facture {
         return "menufact.facture.Facture{" +
                 "date=" + date +
                 ", description='" + description + '\'' +
-                ", state=" + factureState.toString() +
+                ", etat=" + etat +
                 ", platchoisi=" + platchoisi +
                 ", courant=" + courant +
                 ", client=" + client +
@@ -147,22 +152,24 @@ public class Facture {
      *
      * @return une chaîne de caractères avec la facture à imprimer
      */
-    public String genererFacture() {
+    public String genererFacture()
+    {
         String lesPlats = new String();
         String factureGenere = new String();
 
-        int i = 1;
+        int i =1;
 
-        factureGenere = "Facture generee.\n" +
+
+        factureGenere =   "Facture generee.\n" +
                 "Date:" + date + "\n" +
                 "Description: " + description + "\n" +
                 "Client:" + client.getNom() + "\n" +
                 "Les plats commandes:" + "\n" + lesPlats;
 
         factureGenere += "Seq   Plat         Prix   Quantite\n";
-        for (PlatChoisi plat : platchoisi) {
-            factureGenere += i + "     " + plat.getPlat().getDescription() + "  " + plat.getPlat().getPrix() + "      "
-                    + plat.getQuantite() + "\n";
+        for (PlatChoisi plat : platchoisi)
+        {
+            factureGenere +=  i + "     " + plat.getPlat().getDescription() +  "  " + plat.getPlat().getPrix() +  "      " + plat.getQuantite() + "\n";
             i++;
         }
 
@@ -173,3 +180,4 @@ public class Facture {
         return factureGenere;
     }
 }
+
